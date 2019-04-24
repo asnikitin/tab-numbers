ac506538ce0e31e29b2a3c79d2e2aa21a8ebaf24
Andrey Nikitin <drew.oz@yandex.ru>
2019-04-24 20:23:25 +0200

    onUpdated instead hotkey

--- README.md
+++ README.md
@@ -2,28 +2,22 @@
 
 If you are used to **switch between open tabs in Google Chrome** using CMD/CTRL+number of tab (eg. `CMD+4` for tab number 4), you are probably tired of visually counting the tabs you want to switch to. This is what you'll get:
 
-![Preview of tab-numbers in Chrome](https://github.com/eliskah/tab-numbers/blob/master/preview.png)
+![Preview of tab-numbers in Chrome](https://github.com/asnikitin/tab-numbers/blob/master/preview.png)
 
-* Press `ALT+T` to see number of each tab.
-* When you jump there, numbers are removed to free space for original title.
+* Install tab-numbers extension to see number of each tab.
 
 ## Installation
 
 Clone the repository:
 
-    $ git clone https://github.com/eliskah/tab-numbers.git
+    $ git clone https://github.com/asnikitin/tab-numbers.git
 
-![Add tab-numbers to Chrome](https://github.com/eliskah/tab-numbers/blob/master/enable.png)
+![Add tab-numbers to Chrome](https://github.com/asnikitin/tab-numbers/blob/master/enable.png)
 
 In Google Chrome, go to extensions settings (`settings > extensions` or go to `chrome://extensions`). Make sure you have checked developer mode, then `Load unpacked extension..` and navigate to the extension folder.
 
 ## Usage
 
-Once the extension is installed and you have some tabs open, press Alt+T. Titles of tabs will be prepended with the tab index. Please note two things:
+Once the extension is installed and you have some tabs open, titles of tabs will be prepended with the tab index. Please note one thing:
 
 * Some tabs (new tab, extension page, Chrome Webstore page, when you are displaying files etc.) don't have titles altered (at least for now).
-* When focus is on new tab, activating of the function is not working.
-
-After you navigate to any other tab (using CMD/CTRL+index or by clicking), indexes are removed from the title.
-
-If you want to use different keyboard shortcut, navigate to `chrome://extensions/configureCommands` and set it as you like.
--- background.js
+++ background.js
@@ -1,47 +1,52 @@
-var separator = "] "
-var total = 0
-var enabled = true
-
-function indexTitle(index, title, max) {
-
-  var start = title.indexOf(separator) > -1 ? (title.indexOf(separator) + separator.length) : -1
-  var originalTitle = title.substring(start)
-
-  if (enabled == false) { return originalTitle }
-
-  if (index > 7 && index == max-1) { return "9".concat(separator, title) }
-    else if (index > 7) { return "...".concat(separator, title) }
-    else {return "".concat((index+1).toString(), separator, originalTitle)}
+function getTitle({ index, title, total }) {
+  const separator = '] ';
+  const num = index + 1;
+  const nextTitle = title.includes(separator) ? title
+    .split(separator)
+    .slice(1)
+    .join('') : title;
+  if (total > 9) {
+    if (num === total) return `9${separator}${nextTitle}`;
+    if (num > 8) return nextTitle;
+  }
+  return `${num}${separator}${nextTitle}`;
 }
 
 function isValid(url) {
-  valid = true
-  prefixes = ["chrome://", "about:", "view-source:"]
-  for (var i = 0; i < prefixes.length; i++) { if (url.lastIndexOf(prefixes[i]) == 0) { valid = false }}
-  return valid
+  let valid = true;
+  const prefixes = ['chrome://', 'about:', 'view-source:'];
+  prefixes.forEach((item) => {
+    if (url.includes(item)) valid = false;
+  });
+  return valid;
 }
 
-function tabs() {
-  chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, function(tabs) {
-    total = tabs.length
-    for (var i = 0; i < tabs.length; i++) {
-      if (isValid(tabs[i].url)) {
-        chrome.tabs.executeScript(tabs[i].id, { code: "chrome.runtime.sendMessage({'index': ".concat(tabs[i].index, ", 'id': ", tabs[i].id, ", 'title': '", tabs[i].title, "' })") })
+function run() {
+  chrome.tabs.query({ windowId: chrome.windows.WINDOW_ID_CURRENT }, (tabs) => {
+    const total = tabs.length;
+    tabs.forEach((item) => {
+      const { id, url } = item;
+      if (isValid(url)) {
+        chrome.tabs.executeScript(id, {
+          code: `chrome.runtime.sendMessage(${JSON.stringify({
+            ...item,
+            total,
+          })})`,
+        });
       }
-    }
-  }
-)}
+    });
+  });
+}
 
-chrome.commands.onCommand.addListener(function(command) { 
-  enabled = true
-  tabs()
-})
-chrome.tabs.onActivated.addListener(function(tab) { 
-  if (enabled) {
-    enabled = false
-    tabs()
-  }
-})
-chrome.runtime.onMessage.addListener(function(message, sender) {
-  chrome.tabs.executeScript(message.id, { code: "document.title=".concat('"', indexTitle(message.index, message.title, total), '"') } )
-})
+chrome.runtime.onMessage.addListener((data) => {
+  const { id, ...rest } = data;
+  chrome.tabs.executeScript(id, { code: `document.title = ${JSON.stringify(getTitle(rest))}` });
+});
+
+chrome.tabs.onMoved.addListener(() => {
+  run();
+});
+
+chrome.tabs.onUpdated.addListener(() => {
+  run();
+});
